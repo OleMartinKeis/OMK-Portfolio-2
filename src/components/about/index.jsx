48422df94ff181a6b205fd207d3e3f1e3e1ff745
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-12-04 13:53:33 +0100

    Created About page

---
+++ src/components/about/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function About() {
+  return (
+    <div name="about" className="w-full h-screen bg-darkblue text-white">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <h2 className="text-4xl font-bold inline border-b-4 border-pink">
+              About
+            </h2>
+          </div>
+          <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8">
+          <div className="sm:text-right text-4xl ">
+            <p>Hi. I am Ole, nice to meet you. Please take a look around</p>
+          </div>
+          <div>
+            I started web-devleopment two years ago at Noroff School of
+            Technology, since then I have learned to love coding and making
+            websites that are responsive and accessible for everyone! I am just
+            starting my career and would love some feedback from you on my
+            projects.
+          </div>
+        </div>
+        <div className="mt-6">
+          <img
+            src="../public/assets/me.jpeg"
+            className="max-w-[250px] rounded-full border-2 border-orange shadow-2xl"
+            alt="picture of me"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;

43af70256a0d1723149a60c0f49319eb6d5be166
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-12-05 15:15:29 +0100

    Added Work

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,22 @@ import Navbar from "./components/navbar";
 import Home from "./components/home";
 import About from "./components/about";
 import Skills from "./components/skills";
+import Work from "./components/work";
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <Navbar />
-        <Home />
-        <About />
-        <Skills />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <Navbar />
+                <Home />
+                <About />
+                <Skills />
+                <Work />
+            </div>
+        </>
+    );
 }
 
 export default App;
